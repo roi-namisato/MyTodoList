1aa9f4a8be51caebb87ba966d45930558b876a74
Roi Namisato <roi.namisato@g.dreamcareer.co.jp>
2024-05-22 18:27:04 +0900

    更新

--- backend/TodoList/src/main/java/com/example/TodoList/TaskRepository.java
+++ backend/TodoList/src/main/java/com/example/TodoList/TaskRepository.java
@@ -1,6 +1,9 @@
 package com.example.TodoList;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
+	List<Task> findAll();
 }
